b003d068589ff9f8e65eb3b7f571dd8dd500ce24
AslanHuseynov <64832203+AslanHuseynov@users.noreply.github.com>
2020-10-02 20:14:20 +0400

    Update question.js

--- question.js
+++ question.js
@@ -1,4 +1,3 @@
-
 //Array of Objects
 const quiz=
  [
@@ -10,47 +9,47 @@ const quiz=
     {
         q:'Who is the current manager of Manchester United?',
         options:['Soskjaer','Mourinho','Gela','Zidan'],
-        answer:1
+        answer:0
     } ,
     {
         q:'Which legendary actor played Jimmy Hoffa in Martin Scorsese’s 2019 film The Irishman?',
         options:['Will Smith','Al Pacino','Brad Pitt','Jhonny Deep'],
-        answer:2
-    } ,
-    {
-        q:'What does ASP stand for ?',
-        options:['Active Server Pages','All Standard Pages','A Server Page','Active Standard Pages'],
-        answer:4
-    } ,
-    {
-        q:'What does ASP stand for ?',
-        options:['Active Server Pages','All Standard Pages','A Server Page','Active Standard Pages'],
-        answer:2
-    } ,
-    {
-        q:'What does ASP stand for ?',
-        options:['Active Server Pages','All Standard Pages','A Server Page','Active Standard Pages'],
-        answer:3
-    } ,
-    {
-        q:'What does ASP stand for ?',
-        options:['Active Server Pages','All Standard Pages','A Server Page','Active Standard Pages'],
-        answer:3
-    } ,
-    {
-        q:'What does ASP stand for ?',
-        options:['Active Server Pages','All Standard Pages','A Server Page','Active Standard Pages'],
-        answer:4
-    } ,
-    {
-        q:'What does ASP stand for ?',
-        options:['Active Server Pages','All Standard Pages','A Server Page','Active Standard Pages'],
         answer:1
     } ,
     {
-        q:'What does ASP stand for ?',
-        options:['Active Server Pages','All Standard Pages','A Server Page','Active Standard Pages'],
+        q:'Abkhazia is Georia ?',
+        options:['Yes','No'],
+        answer:0
+    } ,
+    {
+        q:'Karabagh is Azerbaijan ?',
+        options:['Yes','No'],
+        answer:0
+    } ,
+    {
+        q:'2+2*2 ?',
+        options:['2','4','8','6'],
+        answer:3
+    } ,
+    {
+        q:'Georgian alphabet is ...',
+        options:['Oldest alphabet','New alphabet'],
+        answer:0
+    } ,
+    {
+        q:'Rustavi is ...',
+        options:['Beatiful city','Dirty city','Fucking shitty city'],
         answer:2
+    } ,
+    {
+        q:'What is difference between niggas and white people ?',
+        options:['Age','Hair','Dick size'],
+        answer:2
+    } ,
+    {
+        q:'Which color is London ?',
+        options:['Blue-Chelsea','Red-4Senal','White-Spurs'],
+        answer:0
     } 
 
  ]
